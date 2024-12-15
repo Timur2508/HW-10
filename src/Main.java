a25f42453734941ee21214f1fb4190c737edba3b
Timur2508 <dispetcher89@list.ru>
2024-12-16 01:36:03 +0500

    DZ на проверку.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание");
+
+        System.out.println("Задача №1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+}
